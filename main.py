1613304913d6d84000c3862adbcb20537492b554
Csokor Rajmond <rajmond@gmail.com>
2026-05-02 08:02:14 +0200

    feat: start employee name input function

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 employees = {}
+overtime_workers = {}
+
+def get_employe_name():
+    while True:
+        employe_name = input("Please enter the name (end with 'done'): ").strip().lower
+        
+        if employe_name == "done":
+            break
+
+        
 
