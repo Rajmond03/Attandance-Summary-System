89084996562765e6f4e296e1a92f062b21ed2c11
Csokor Rajmond <rajmond@gmail.com>
2026-05-02 10:42:28 +0200

    feat: add employee name input

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ overtime_workers = {}
 
 def get_employe_name():
     while True:
-        employe_name = input("Please enter the name (end with 'done'): ").strip().lower
+        employee_name = input(
+            "Please enter the name (end with 'done'): "
+            ).strip().lower
         
-        if employe_name == "done":
-            break
+        if any(char.isdigit() for char in employee_name):
+            print("Only letters are allowed!")
+            continue
+        
+        elif employee_name == "done":
+            return None
+        
+        else:
+            return employee_name
 
-        
 
